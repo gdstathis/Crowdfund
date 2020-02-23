032989fa562d94c960d8961ee97ca4b5389e82ba
Evangeliazisouli <evaggliazisouli@gmail.com>
2020-02-23 10:00:01 +0200

    .Add ApiesultExtensions

---
+++ Crowdfund.Web/Extensions/ApiResultExtensions.cs
@@ -0,0 +1,26 @@
+﻿using CrowdfundCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crowdfund.Web.Extensions
+{
+    public static class ApiResultExtensions
+    {
+        public static ObjectResult AsStatusResult<T>(
+           this ApiResult<T> @this)
+        {
+            var result = default(ObjectResult);
+
+            if (@this.Success)
+            {
+                result = new ObjectResult(@this.Data);
+            } else
+            {
+                result = new ObjectResult(@this.ErrorText);
+            }
+
+            result.StatusCode = (int)@this.ErrorCode;
+
+            return result;
+        }
+    }
+}
